17a8a3d3756a74eb7330fccfb01f22f96c84425e
Utkarsh <utkarshSrivastava07@outlook.com>
2022-11-05 12:50:03 +0530

    First Merge

--- hello.java
+++ hello.java
@@ -1,5 +1,6 @@
 public class hello{
     public static void main(String args[]){
-        System.out.print("Hello World");
+        System.out.print("Hello World \n");
+        System.out.println("This is First added here");
     }
 }
