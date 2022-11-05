38ef5bd9150fb4c435ab5b9b4c4cd82e83a5b129
Utkarsh <utkarshSrivastava07@outlook.com>
2022-11-05 13:19:13 +0530

    Added Second Class

---
+++ Arrays/declaring_Array1.java
@@ -0,0 +1,12 @@
+
+public class declaring_Array1 {
+    public static void main(String[] args) {
+        int[] ar = new int[5];
+        ar[0] = 1;
+        ar[1] =2;
+        ar[2]=3;
+        ar[3]=4;
+        ar[4]=5;
+        System.out.println(ar[4]);
+    }
+}
